54caaefa025f777ebee310d0eee31067e8a45506
Yanislav Ivanov <frexcho998@gmail.com>
2021-07-26 00:15:51 +0300

    Added Api controller

---
+++ BulgarianProducers/Controllers/Api/ProductsAndServicesApiController.cs
@@ -0,0 +1,32 @@
+﻿using BulgarianProducers.Models;
+using BulgarianProducers.Services.Contracts;
+using BulgarianProducers.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianProducers.Controllers.Api
+{
+    [ApiController]
+    [Route("/api/productsAndServices")]
+    public class ProductsAndServicesApiController:ControllerBase
+    {
+        private readonly IGetServicesAndProductsService getServicesAndProducts;
+
+        public ProductsAndServicesApiController(IGetServicesAndProductsService getServicesAndProducts)
+        {
+            this.getServicesAndProducts = getServicesAndProducts;
+        }
+
+        [HttpGet]
+        public ProductsAndServicesQueryModel All([FromQuery] ProductsAndServicesApiRequest queryModel)
+            => this.getServicesAndProducts.GetServicesAndProducts(
+                queryModel.SearchTerm,
+                queryModel.Sorting, 
+                queryModel.ShowProducts, 
+                queryModel.ShowServices, 
+                queryModel.CurrentPage);
+    }
+}
---
+++ BulgarianProducers/Models/ProductsAndServicesApiRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static BulgarianProducers.Infrastructure.Constants;
+
+namespace BulgarianProducers.Models
+{
+    public class ProductsAndServicesApiRequest
+    {
+        public string SearchTerm { get; set; }
+        public ProductsAndServicesSorting Sorting { get; set; }
+        public bool ShowProducts { get; set; }
+        public bool ShowServices { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        
+
+    }
+}
