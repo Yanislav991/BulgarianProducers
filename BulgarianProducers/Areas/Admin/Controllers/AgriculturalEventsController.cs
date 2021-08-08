eba5affef7fe819c7671fefdb3f0865431bd6c7f
Yanislav Ivanov <frexcho998@gmail.com>
2021-08-08 23:31:43 +0300

    Added admin and admin area, plus antiforgery token

---
+++ BulgarianProducers/Areas/Admin/Controllers/AgriculturalEventsController.cs
@@ -0,0 +1,33 @@
+﻿using BulgarianProducers.Services.Contracts;
+using BulgarianProducers.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulgarianProducers.Areas.Admin.Controllers
+{
+    public class AgriculturalEventsController : AdminController
+    {
+        private readonly IEventsService eventsService;
+
+        public AgriculturalEventsController(IEventsService eventService)
+        {
+            this.eventsService = eventService;
+        }
+
+        public IActionResult AllEvents([FromQuery] AgriculturalEventQueryModel queryModel) 
+        {
+            var queryResult = this.eventsService.GetEvents(
+                queryModel.SearchTerm,
+                queryModel.Place,
+                queryModel.StartAfter,
+                queryModel.EndBefore,
+                queryModel.CurrentPage,
+                queryModel.Sorting);
+
+            return this.View(queryResult);
+        }
+    }
+}
--- BulgarianProducers/Startup.cs
+++ BulgarianProducers/Startup.cs
@@ -6,10 +6,13 @@ using BulgarianProducers.Services.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace BulgarianProducers
 {
@@ -30,6 +33,7 @@ namespace BulgarianProducers
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
+            services.AddAuthorization(x => x.AddPolicy("RequireAdministratorRole", x => x.RequireRole("Admin")));
             services.AddDefaultIdentity<User>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
@@ -38,8 +42,9 @@ namespace BulgarianProducers
                 options.Password.RequireUppercase = false;
                 
             })
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<BulgarianProducersDbContext>();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(x=>x.Filters.Add<AutoValidateAntiforgeryTokenAttribute>());
 
             services.AddTransient<IGetServicesAndProductsService, GetServicesAndProductsService>();
           
@@ -50,7 +55,7 @@ namespace BulgarianProducers
             services.AddTransient<IServicesService, ServicesService>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             app.PrepareDatabase();
             if (env.IsDevelopment())
@@ -71,11 +76,58 @@ namespace BulgarianProducers
             .UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                name: "Areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+            CreateRoles(serviceProvider);
             
+        }
+        private void CreateRoles(IServiceProvider serviceProvider)
+        {
+
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+            Task<IdentityResult> roleResult;
+            string email = "admin@gmail.com";
+
+            //Check that there is an Administrator role and create if not
+            Task<bool> hasAdminRole = roleManager.RoleExistsAsync("Admin");
+            hasAdminRole.Wait();
+
+            if (!hasAdminRole.Result)
+            {
+                roleResult = roleManager.CreateAsync(new IdentityRole("Admin"));
+                roleResult.Wait();
+            }
+
+            //Check if the admin user exists and create it if not
+            //Add to the Administrator role
+
+            Task<User> testUser = userManager.FindByEmailAsync(email);
+            testUser.Wait();
+
+            if (testUser.Result == null)
+            {
+                User administrator = new User();
+                administrator.Email = email;
+                administrator.UserName = "admin";
+                
+
+                Task<IdentityResult> newUser = userManager.CreateAsync(administrator, "123456a");
+                newUser.Wait();
+
+                if (newUser.Result.Succeeded)
+                {
+                    Task<IdentityResult> newUserRole = userManager.AddToRoleAsync(administrator, "Admin");
+                    newUserRole.Wait();
+                }
+            }
+
         }
     }
 }
